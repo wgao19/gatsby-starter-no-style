3b6f78220d00b644aeb755b1edc9b8522c901f5d
Wei Gao <wgao19@gmail.com>
2019-08-18 03:50:07 +0000

    favicon

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Gatsby no-style starter.
 
 Initially created for [Styling Gatsby Site](#) <!-- TODO: put link to slides? --> workshop for Women Who Code [Connect Asia](https://wwcodeconnectasia.github.io/).
+
+## Credits
+
+Site's favicon by [Daniel Bruce](https://www.flaticon.com/authors/daniel-bruce) on [FlatIcon](www.flaticon.com) under [Creative Commons BY 3.0](http://creativecommons.org/licenses/by/3.0/).
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,38 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter No Style'
-  }
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/static/favicon.png',
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
+  ]
 };
